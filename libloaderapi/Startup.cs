e3e7758467e5e9a043fd0b9b253fff28e59ce38c
mihaly044 <mihaly044@gmail.com>
2020-08-21 19:35:02 +0200

    Automatically migrate DB on startup

--- libloaderapi/Startup.cs
+++ libloaderapi/Startup.cs
@@ -87,8 +87,10 @@ namespace libloaderapi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
