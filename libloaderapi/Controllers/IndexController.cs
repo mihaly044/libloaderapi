1f2b3324a21718ea50b155681e2d1f12f09fc9af
mihaly044 <mihaly044@gmail.com>
2020-08-25 21:41:23 +0200

    Return teapot result

---
+++ libloaderapi/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace libloaderapi.Controllers
+{
+    [Route("")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public Task<string> Get()
+        {
+            // This is going to be funny
+            Response.StatusCode = 418;
+            return Task.FromResult("I'm a teapot (really!)");
+        }
+    }
+}
