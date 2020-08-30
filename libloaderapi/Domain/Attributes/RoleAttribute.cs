11381dffcf5ceba906563e95651e87fbd0ed4574
mihaly044 <mihaly044@gmail.com>
2020-08-30 21:38:19 +0200

    Removed redundant assignment

--- libloaderapi/Domain/Attributes/RoleAttribute.cs
+++ libloaderapi/Domain/Attributes/RoleAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace libloaderapi.Domain.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class RoleAttribute : AuthorizeAttribute
     {
         public RoleAttribute(params string[] roles)
