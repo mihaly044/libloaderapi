5cf1748b8e3c193891fa689c6be0045338e0129b
mihaly044 <mihaly044@gmail.com>
2020-08-31 01:33:05 +0200

    Added login command

---
+++ libloaderapi-cli/Commands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace libloaderapi_cli
+{
+    [Verb("login")]
+    public class LoginCommand
+    {
+        [Option('u', "username", Required = true, HelpText = "This is your user name.")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "This is your password.")]
+        public string Password { get; set; }
+
+        [Option('d', "drop-token", Required = false, HelpText = "The authorization token will not be saved for later use. " +
+                                                                "This means you will have to log in each time you perform an action.", Default = false)]
+        public bool DropToken { get; set; }
+    }
+}
--- libloaderapi-cli/Program.cs
+++ libloaderapi-cli/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+using libloaderapi.Common.Dto.Auth;
 
 namespace libloaderapi_cli
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This one client is going to be used throughout the entire lifetime of the app
+        /// </summary>
+        private static readonly HttpClient Client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello Woreld!");
+            // Initialize the client
+            Client.BaseAddress = new Uri("https://api.libloader.net");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("text/plain"));
+
+            // Parse the login command
+            await Parser.Default.ParseArguments<LoginCommand>(args).WithParsedAsync(async opts =>
+            {
+                var response = await Client.PostAsJsonAsync("/users/authenticate", new AuthRequest
+                {
+                    Username = opts.Username,
+                    Password = opts.Password
+                });
+
+                var result = await response.Content.ReadAsAsync<AuthResult>();
+
+                if (result.Success)
+                {
+                    var handler = new JwtSecurityTokenHandler();
+                    var jwtSecurityToken = handler.ReadToken(result.Token) as JwtSecurityToken;
+
+
+                    Console.WriteLine($"Token will expire at UTC {jwtSecurityToken.ValidTo}");
+                    Console.WriteLine("Your token has the following claims:");
+
+                    foreach (var claim in jwtSecurityToken.Claims.Select(x => $"{x.Type}={x.Value}"))
+                    {
+                        Console.WriteLine(claim);
+                    }
+
+                    Console.WriteLine($"\r\nToken={result.Token}");
+
+
+                    /*if (!opts.DropToken)
+                    {
+                        await File.WriteAllTextAsync("user_token.txt", result.Token);
+                    }*/
+                }
+                else
+                {
+                    Console.WriteLine($"{response.StatusCode}, {result.Message}");
+                }
+            });
         }
     }
 }
