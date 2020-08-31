fdda39adcc47b979871e60c89e88d5d47552cf75
mihaly044 <mihaly044@gmail.com>
2020-08-31 13:06:51 +0200

    Implemented the Time getter

--- libloaderapi/Controllers/ClientsController.cs
+++ libloaderapi/Controllers/ClientsController.cs
@@ -93,6 +93,13 @@ namespace libloaderapi.Controllers
             return BadRequest("400 Bad request");
         }
 
+        [HttpGet("time")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<DateTime> Time()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+
         private string GetIpAddress()
         {
             return HttpContext.Request.Headers.TryGetValue("X-Real-IP", out var ipAddress)
